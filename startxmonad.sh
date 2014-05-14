09daf4359396184252c2441c6a0f9f237a5de81a
Radu Voicilas <radu.voicilas@gmail.com>
2014-05-15 00:35:50 +0300

    Add tray to xmonad

---
+++ startxmonad.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+stalonetray &
+exec xmonad
--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,14 @@
 import XMonad
+import XMonad.Hooks.ManageDocks
+
+myManagementHooks :: [ManageHook]
+myManagementHooks = [
+  resource =? "stalonetray" --> doIgnore
+  ]
 
 main = xmonad defaultConfig
     { modMask = mod4Mask
     , terminal = "konsole"
     , focusFollowsMouse = False
+    , manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
     }
