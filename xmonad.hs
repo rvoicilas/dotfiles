26b0850aa9339abd7cc2a0953e02ae1290659a37
Radu Voicilas <radu.voicilas@gmail.com>
2014-08-09 17:46:50 +0300

    Add xmobar

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,25 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
 myManagementHooks :: [ManageHook]
 myManagementHooks = [
   resource =? "stalonetray" --> doIgnore
   ]
 
-main = xmonad defaultConfig
-    { modMask = mod4Mask
-    , terminal = "konsole"
-    , focusFollowsMouse = False
-    , manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
-    }
+main = do
+    xmproc <- spawnPipe "xmobar"
+
+    xmonad $ defaultConfig
+        { modMask = mod4Mask
+        , terminal = "konsole"
+        , focusFollowsMouse = False
+        , manageHook = manageDocks <+> manageHook defaultConfig <+> composeAll myManagementHooks
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        }
